e3d5f7e86b2a1940642c3a5d338ab0f1e11c112a
srdjan <srdjan@timok.com>
2014-11-27 15:38:24 -0500

    working on windows

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 var React = require('react');
 var pkg = require('./package.json');
 
-React.renderComponent(<h1>{pkg.name}, brought to you by React!</h1>, document.body);
+React.render(<h1>{pkg.name}, brought to you by React!</h1>, document.body);
